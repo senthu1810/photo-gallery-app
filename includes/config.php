cd21f55f465b7f7705e6ecc86e3198341f8443ca
senthu1810 <senthuransenthu1810@gmail.com>
2025-08-20 14:57:59 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";   // default XAMPP username
+$pass = "";       // default XAMPP password (empty)
+$dbname = "photo_gallery_app";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
